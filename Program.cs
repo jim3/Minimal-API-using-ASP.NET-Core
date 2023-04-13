10d57f9ebcd82f87359c1767c0c4a3f69d9f1e47
jim3 <j3.casey@protonmail.com>
2023-04-13 17:18:54 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,80 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System.Text.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+// Sample parts database
+var parts = new[] {
+    new { Name = "screws", Type = "wood", Quantity = 100, Price = 0.12m },
+    new { Name = "nuts", Type = "hex", Quantity = 150, Price = 0.18m },
+    new { Name = "bolts", Type = "eye", Quantity = 75, Price = 0.15m },
+    new { Name = "washers", Type = "flat", Quantity = 200, Price = 0.12m },
+    new { Name = "bolts", Type = "carriage", Quantity = 25, Price = 0.19m }
+};
+
+// Get all parts
+app.MapGet("/api/parts", () => {
+    return Results.Json(parts);
+});
+
+// Create new part
+app.MapPost("/api/parts", (string partname, string parttype, int quantity, decimal price) => {
+    var newPart = new {
+        Name = partname,
+        Type = parttype,
+        Quantity = quantity,
+        Price = price
+    };
+
+    return Results.Json(newPart);
+});
+
+// Get a specific part
+app.MapGet("/api/parts/{partname}", (string partname) => {
+    var part = parts.FirstOrDefault(p => p.Name == partname);     // Find the part
+
+    if (part == null) {
+        return Results.NotFound();
+    }
+
+    return Results.Json(part);
+});
+
+// Update a specific part
+app.MapPut("/api/parts/{partname}", (string partname, string parttype, int quantity, decimal price) => {
+    var part = parts.FirstOrDefault(p => p.Name == partname); // Find the part
+
+    if (part == null) {
+        return Results.NotFound();
+    }
+
+    // Create a new anonymous object with the updated values
+    var updatedPart = new {
+        Name = partname,
+        Type = parttype,
+        Quantity = quantity,
+        Price = price
+    };
+
+    // Update part
+    parts = parts.Where(p => p.Name != partname).ToArray();
+    parts.Append(updatedPart);
+
+    return Results.Json(updatedPart);
+});
+
+// Delete a specific part
+app.MapDelete("/api/parts/{partname}", (string partname) => {
+    var part = parts.FirstOrDefault(p => p.Name == partname);   // Find the part
+
+    if (part == null) {
+        return Results.NotFound();
+    }
+
+    parts = parts.Where(p => p.Name != partname).ToArray(); // Remove the part from the array
+    return Results.Json(part);     // Return the deleted part as JSON
+});
+
+app.Run();
---
+++ README.md
@@ -0,0 +1,12 @@
+## C# / .NET Restful API for an Inventory Management System
+
+### Description
+
+The project is a Restful API for an Inventory Management System. It is a simple inventory management system that allows users to create, read, update, and delete items in inventory. It is written in C# and uses the .NET Core framework. Like the Flask (and Go) project, this project was created to see how different each of these frameworks were to work with. In the end, they are a fairly similar, at least on a basic level. Suprisingly, as much as I've grown to like JavaScript, I think I like C# the most. It's a very clean language, has proper classes and it's strongly typed, which I have a preference for. I am not curious how it handles asynchronous code. I think I'll be using C# more in the future.
+
+### Installation (assuming you have .NET Core installed)
+
+1. Clone the repository from GitHub using git clone.
+2. Open the cloned repository in Visual Studio Code or another code editor.
+3. Open a terminal in the root directory of the project and run the command `dotnet run` to start the API.
+4. The API should now be accessible at `http://localhost:5000/api/parts` in a web browser or via an API client like [Postman](https://www.postman.com)
